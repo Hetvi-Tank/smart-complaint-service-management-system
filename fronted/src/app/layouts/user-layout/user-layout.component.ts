c899ba2a619da770ac5ddaec5218b565092cb367
Hetvi-Tank <hetvitank24@gmail.com>
2026-03-05 09:06:05 +0530

    add logo

--- fronted/src/app/layouts/user-layout/user-layout.component.ts
+++ fronted/src/app/layouts/user-layout/user-layout.component.ts
@@ -6,37 +6,8 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   selector: 'app-user-layout',
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink],
-  template: `
-    <nav class="navbar">
-      <div>
-        <!-- <a routerLink="/user/dashboard">Dashboard</a>
-        <a routerLink="/user/add">Add Complaint</a> -->
-        <a routerLink="/login">Logout</a>
-      </div>
-    </nav>
-
-    <div class="content">
-      <router-outlet></router-outlet>
-    </div>
-  `,
-  styles: [`
-    .navbar {
-      background: #16a085;
-      padding: 15px;
-      display: flex;
-      justify-content: space-between;
-      color: white;
-    }
-
-    .navbar a {
-      margin-left: 15px;
-      color: white;
-      text-decoration: none;
-    }
-
-    .content {
-      padding: 30px;
-    }
-  `]
+  templateUrl: './user-layout.component.html',
+  styleUrls: ['./user-layout.component.css']
 })
+
 export class UserLayoutComponent {}
